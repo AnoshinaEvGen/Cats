1c3b48b12b48eaca9997477627b7f610665397a8
AnoshinaEvGen <mega-feya@mail.ru>
2025-07-31 19:27:13 +0300

    Создана функция для получения изображения с котиком

--- Cats.py
+++ Cats.py
@@ -1,8 +1,21 @@
+from idlelib.rpc import response_queue
 from tkinter import *
 from PIL import Image, ImageTk
 import requests
 from pygame.examples.aliens import load_image
 from to import BytestIO
+
+def loade_image():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytestIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f'Произошла ошибка: {e}')
+        return None
+
 
 window = Tk()
 window.title("Cats")
