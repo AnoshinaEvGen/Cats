6df4cd2c0d07b6f5a681cae1e6d14cb2f709129f
AnoshinaEvGen <mega-feya@mail.ru>
2025-08-01 13:04:01 +0300

    Добавлено получение изображений по тегу/без тега

--- Cats.py
+++ Cats.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+
 def load_image(url):
     try:
         response = requests.get(url)
@@ -15,21 +16,21 @@ def load_image(url):
         print(f'Произошла ошибка: {e}')
         return None
 
+
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
+    img = load_image(url_tag)
+
+
     if img:
         new_window = Toplevel()
         new_window.title('Картинка с котиком')
-        new_window.geometry('600x480')
-        label.image = img
+        new_window.geometry('600x580')
+        label = Label(new_window, image=img)
         label.pack()
-
-def set_image():
-    img = load_image(url)
-
-    if img:
-        label.config(image= img)
         label.image = img
+
 
 def exit():
     window.destroy()
@@ -37,31 +38,29 @@ def exit():
 
 window = Tk()
 window.title("Cats")
-window.geometry("600x520")
+window.geometry("600x80")
 
-label = Label()
-label.pack()
+tag_entry = Entry()
+tag_entry.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
+
 
 # update_button = Button(text="Обновить", command=set_image)
 # update_button.pack()
 
 menu_bar = Menu(window)
-window.config(menu = menu_bar)
+window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label='Файл', menu=file_menu)
-file_menu.add_command(label='Загрузить фото', command=set_image)
+file_menu.add_command(label='Загрузить фото', command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label='Выход', command=exit)
 
+
+
 url = "https://cataas.com/cat"
-img = load_image(url)
-
-if img:
-    label.config(image=img)
-    label.image = img
-
-set_image()
 
 window.mainloop()
-
