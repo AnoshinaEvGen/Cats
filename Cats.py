28b7d8f0d75aefd4e7de2496bf4ba75ceff699ae
AnoshinaEvGen <mega-feya@mail.ru>
2025-08-01 09:34:27 +0300

    Добавляем кнопку для загрузки нового изображения

--- Cats.py
+++ Cats.py
@@ -1,35 +1,46 @@
-from idlelib.rpc import response_queue
 from tkinter import *
 from PIL import Image, ImageTk
 import requests
-from pygame.examples.aliens import load_image
-from to import BytestIO
+from io import BytesIO
 
-def loade_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
-        image_data = BytestIO(response.content)
+        image_data = BytesIO(response.content)
         img = Image.open(image_data)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f'Произошла ошибка: {e}')
         return None
 
+    def set_image:
+        img = load_image(url)
+
+        if img:
+            label.config(image=img)
+            label.image = img
+
+
 
 window = Tk()
 window.title("Cats")
-window.geometry("600*400")
+window.geometry("600x400")
 
 label = Label()
 label.pack()
+
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
 
 url = "https://cataas.com/cat"
 img = load_image(url)
 
 if img:
-    label.config(image=img)
+    label.config(image = img)
     label.image = img
+
+set_image()
 
 window.mainloop()
 
